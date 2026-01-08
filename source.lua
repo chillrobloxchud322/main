7c49e36d8f166a932d2a64b2715fcd82299cf61d
chillrobloxchud322 <yodieland.com@gmail.com>
2026-01-08 14:43:42 -0400

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,471 @@
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local LocalPlayer = Players.LocalPlayer
+local Debris = game:GetService("Debris")
+local function BindCooldownToMove(move,Callback)
+	local cooldown = move:GetAttributeChangedSignal("COOLDOWN"):Connect(function()
+		if move:GetAttribute("COOLDOWN") == 1 then
+			return Callback()
+		end
+	end)
+end
+
+local Target_Anims = {
+    ["rbxassetid://1461128166"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1461128859"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1461136273"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1461136875"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1470422387"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1470439852"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1470449816"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1470447472"] = {
+        delay = 0.1,
+        Scaling = 3,
+    },
+    ["rbxassetid://1461145506"] = {
+        delay = 0.1,
+        Scaling = 2
+    }
+}
+
+local Logged = {}
+
+local function BindRemoteToMove(moveName, callback)
+    Logged[moveName] = callback
+end
+
+local oldNamecall
+oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    local args = { ... }
+
+    if not checkcaller()
+        and method == "FireServer"
+        and self.Name == "Input"
+        and args[1] == "fire"
+        and typeof(args[2]) == "table"
+        and args[2].ToolName
+        and Logged[args[2].ToolName]
+    then
+        Logged[args[2].ToolName](args[2],oldNamecall(self, ...))
+    end
+
+    return oldNamecall(self, ...)
+end))
+
+
+local Camera = game.Workspace.Camera
+local OldFov = Camera.FieldOfView
+local player = Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+
+local function FindNearestLive(NearMouse)
+        local closestHRP = nil
+        local closestDist = math.huge
+        
+        for _, v in ipairs(workspace.Live:GetChildren()) do
+            if v.Name ~= player.Name and v:FindFirstChild("HumanoidRootPart") then
+                local dist = (v.HumanoidRootPart.Position - Camera.CFrame.Position).Magnitude
+                if NearMouse then
+                    local Mouse = game.Players.LocalPlayer:GetMouse()
+                    dist = (v.HumanoidRootPart.Position - Mouse.Hit.Position).Magnitude
+                end
+
+                if dist < 100 and dist < closestDist then
+                    closestDist = dist
+                    closestHRP = v.HumanoidRootPart
+                end
+            end
+        end
+    return closestHRP
+end
+
+local Window = Library:CreateWindow({
+    Title = 'ABA X 67',
+    Center = true,
+    AutoShow = true,
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['UI Settings'] = Window:AddTab('UI Settings'),
+}
+
+local MainBox = Tabs.Main:AddLeftGroupbox('PVP')
+
+MainBox:AddToggle('Upfling', {
+    Text = 'Upfling Extend',
+    Default = false
+})
+
+MainBox:AddToggle('Backugo', {
+    Text = 'Backugo Damage Aura',
+    Tooltip = 'press reset to speed up',
+    Default = false
+})
+
+MainBox:AddToggle('RocketTp', {
+    Text = 'Raiden Rocket Tp',
+    Tooltip = 'look down after pressing 2',
+    Default = false
+})
+
+MainBox:AddToggle('AP', {
+    Text = 'Auto Block M1s',
+    Tooltip = 'really bad',
+    Default = false
+})
+local Hitboxes = {}
+MainBox:AddToggle('Hitboxes', {
+    Text = 'show m1 hitboxes',
+    Tooltip = 'm1 hitboxes',
+    Default = false
+})
+
+local tpwalking = false
+MainBox:AddToggle('NoStun', {
+    Text = 'No Stun',
+    Default = false
+})
+
+MainBox:AddToggle('AutoBlackFlash', {
+    Text = 'Auto Black Flash',
+    Default = false
+})
+
+MainBox:AddToggle('NanamiAuto', {
+    Text = 'Nanami Auto',
+    Default = false
+})
+MainBox:AddToggle('sandtp',{
+    Text = 'Insant Sand Coffin',
+    Default = false
+})
+MainBox:AddToggle('birdtp',{
+    Text = 'TP Deidara Bird',
+    Default = false
+})
+
+MainBox:AddToggle('kitp',{
+    Text = 'TP Scattershot',
+    Default = false
+})
+MainBox:AddButton({
+    Text = 'Reset',
+    Func = function()
+        LocalPlayer.Character:WaitForChild("Humanoid").Health = 0
+    end
+})
+
+
+
+MainBox:AddButton({
+    Text = 'Morel Void',
+    Func = function()
+            BindCooldownToMove(LocalPlayer.Backpack:FindFirstChild('Smoky Chain'),function()
+            wait(1)
+            local hrp = char:WaitForChild("HumanoidRootPart")
+            local originalCFrame = hrp.CFrame
+            hrp.CFrame = originalCFrame * CFrame.new(1, 9e9, 0)
+            wait(1)
+
+            task.wait(10)
+            hrp.CFrame = originalCFrame
+        end)
+    end
+})
+
+local TP_UP_TIME = 0.03
+local TP_DOWN_TIME = 0.5
+
+local watchedAnimations = {
+    ["rbxassetid://1461252313"] = 0.9,
+    ["none"] = 0.9
+}
+
+
+
+local function Parry(held)
+    local char = LocalPlayer.Character
+    if not char then return end
+    local remote = LocalPlayer.Backpack.ServerTraits:FindFirstChild('Input')
+    if not remote then return end
+
+    remote:FireServer("f")
+    task.wait(held or 0.2)
+    remote:FireServer("foff")
+end
+
+local function CreateHitBox(root,config)
+    task.wait(config.delay)
+
+    if not root or not root.Parent then return end
+
+    local Hitbox = Instance.new("Part")
+    Hitbox.Size = Vector3.new(4.7, 4.7, 4.7) * (config.Scaling or 1)
+    Hitbox.Material = Enum.Material.ForceField
+    Hitbox.Shape = Enum.PartType.Ball
+    Hitbox.CanCollide = false
+    Hitbox.Massless = true
+    Hitbox.Anchored = false
+    Hitbox.CFrame = root.CFrame * CFrame.new(0, 0, (config.offset or -3.5) )
+    Hitbox.Parent = workspace
+    Hitbox.BrickColor = BrickColor.new("Really red")
+    if not Toggles.Hitboxes.Value then
+        Hitbox.Transparency = 1
+    end
+
+    local weld = Instance.new("WeldConstraint")
+    weld.Part0 = Hitbox
+    weld.Part1 = root
+    weld.Parent = Hitbox
+
+    Hitboxes[Hitbox] = true
+
+    Debris:AddItem(Hitbox, 0.15)
+    task.delay(0.15, function()
+        Hitboxes[Hitbox] = nil
+    end)
+end
+
+local function addedChild(child)
+    if not child:IsA("Model") then return end
+
+    local humanoid = child:FindFirstChildOfClass("Humanoid")
+    local root = child:FindFirstChild("HumanoidRootPart")
+    if not humanoid or not root then return end
+    
+    humanoid.AnimationPlayed:Connect(function(track)
+        local anim = track.Animation
+        if not anim then return end
+
+        local data = Target_Anims[anim.AnimationId]
+        if not data then return end
+
+        CreateHitBox(root,{
+            delay = data.delay,
+            Scaling = data.Scaling
+        })
+    end)
+end
+
+BindRemoteToMove("Sand Coffin", function(data,callback)
+    if Toggles.sandtp.Value then
+    wait(0.35)
+    for i,v in pairs(workspace:GetDescendants()) do
+    if v:IsA('BasePart') and isnetworkowner(v) then
+        if v.Name == 'SwipeCloud' then
+            v.CFrame = FindNearestLive(true).CFrame
+        end 
+    end
+    end
+    end
+    return callback
+end)
+
+BindRemoteToMove("Scattershot", function(data,callback)
+    if Toggles.kitp.Value then
+    wait(0.35)
+    for i,v in pairs(workspace:GetDescendants()) do
+    if v:IsA('BasePart') and isnetworkowner(v) then
+        if v.Name == 'KiBlast' then
+            v.CFrame = FindNearestLive(true).CFrame
+        end 
+    end
+    end
+    end
+    return callback
+end)
+
+BindRemoteToMove("Spirit Shotgun", function(data,callback)
+    if Toggles.kitp.Value then
+    wait(0.45)
+    for i,v in pairs(workspace.Thrown:GetDescendants()) do
+    if v:IsA('BasePart') and isnetworkowner(v) then
+        if v.Name == 'KiBlast' then
+        task.defer(function()
+        for i = 1,100 do
+            v.CFrame = FindNearestLive(true).CFrame
+            wait(0.02)
+        end
+            end)
+        end 
+    end
+    end
+    end
+    return callback
+end)
+
+BindRemoteToMove("Bird", function(data,callback)
+    if Toggles.birdtp.Value then
+    wait(0.55)
+    for i,v in pairs(workspace.Thrown:GetDescendants()) do
+    if v:IsA('BasePart') and isnetworkowner(v) then
+        if v.Parent.Name == 'Bird' then
+        local targetC = FindNearestLive(true).CFrame
+        task.defer(function()
+        for i = 1,50 do
+            v.CFrame = targetC
+            wait(0.02)
+        end
+            end)
+        end 
+    end
+    end
+    end
+    return callback
+end)
+
+local function onCharacter(char)
+    local humanoid = char:WaitForChild("Humanoid")
+    local root = char:WaitForChild("HumanoidRootPart")
+
+    root.Touched:Connect(function(part)
+        if Hitboxes[part] and Toggles.AP.Value then
+            Parry(0.2)
+        end
+    end)
+    humanoid.AnimationPlayed:Connect(function(track)
+        if not Toggles.Upfling.Value then return end
+        if not track.Animation then return end
+
+        local delayTime = watchedAnimations[track.Animation.AnimationId]
+        if not delayTime then return end
+        local closestHRP = FindNearestLive()
+        if not closestHRP then return end
+        task.wait(delayTime)
+
+        a = 15 
+        b,c = char.HumanoidRootPart.Position.X,char.HumanoidRootPart.Position.Z 
+        char.HumanoidRootPart.CFrame = closestHRP.CFrame * CFrame.new(0, a, 0) 
+        char.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(b,char.HumanoidRootPart.Position.Y,c)) 
+    end)
+
+     local hrp = char:WaitForChild("HumanoidRootPart")
+    local live = workspace:WaitForChild("Live")
+    local auto = live.DescendantAdded:Connect(function(desc)
+        if not Toggles.NanamiAuto.Value then return end
+	    if desc.Name == "NanamiCutGUI" then
+		    local gui = desc
+		    local mainBar = gui:WaitForChild("MainBar")
+		    local cutter = mainBar:WaitForChild("Cutter")
+	        local goal = mainBar:WaitForChild("Goal")
+	        local connection
+	        connection = game:GetService("RunService").RenderStepped:Connect(function()
+	            local cutterX = cutter.Position.X.Scale
+                local goalX = goal.Position.X.Scale
+	            if math.abs(cutterX - goalX) <= 0.016 then
+			        mouse1click()
+			        connection:Disconnect()
+			        connection = nil
+			    end
+		    end)
+        end
+    end)
+    humanoid.Died:Connect(function()
+        auto:Disconnect()
+    end)
+end
+
+
+if LocalPlayer.Character then
+    onCharacter(LocalPlayer.Character)
+end
+
+for _, v in ipairs(workspace.Live:GetChildren()) do
+    addedChild(v)
+end
+
+workspace.Live.ChildAdded:Connect(addedChild)
+
+if LocalPlayer.Character then
+    onCharacter(LocalPlayer.Character)
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacter)
+
+RunService.RenderStepped:Connect(function()
+    if Toggles.Backugo.Value then
+        local traits = LocalPlayer.Backpack:FindFirstChild("ServerTraits")
+        if traits and traits:FindFirstChild("Input") then
+            traits.Input:FireServer("dodge", { explod = Enum.KeyCode.W })
+        end
+    end
+    if Toggles.AutoBlackFlash.Value then
+        if Camera.FieldOfView >= 40 and Camera.FieldOfView < 70 and Camera.FieldOfView > OldFov then
+		    wait(0.01)
+            print('CLICK')
+		    mouse1click()
+	    end
+	    OldFov = Camera.FieldOfView
+    end
+    if Toggles.NoStun.Value then
+        for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+            if v:IsA('BodyVelocity') then
+                v.MaxForce = Vector3.new(0,9e6,0)
+            end
+        end
+    end
+    if Toggles.RocketTp.Value then
+        for i,v in pairs(workspace.Thrown:GetChildren()) do
+            if v.Name ~= 'Missile' then continue end
+            if v:IsA('BasePart') and isnetworkowner(v) then
+                if FindNearestLive() then
+                    v.CFrame = FindNearestLive().CFrame
+                end 
+            end
+        end
+    end
+end)
+
+
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function()
+    Library:Unload()
+end)
+
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', {
+    Default = 'End',
+    NoUI = true,
+    Text = 'Menu keybind'
+})
+
+Library.ToggleKeybind = Options.MenuKeybind
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+
+ThemeManager:SetFolder('ABA_X_67')
+SaveManager:SetFolder('ABA_X_67/configs')
+
+SaveManager:BuildConfigSection(Tabs['UI Settings'])
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
